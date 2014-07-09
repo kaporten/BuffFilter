61e697ecc960eb68dbeb5e2fe38fe662bfb76a81
porten <porten@gmail.com>
2014-07-09 19:53:38 +0200

    Performance tuning in central loop

--- BuffFilter.lua
+++ BuffFilter.lua
@@ -188,44 +188,40 @@ function BuffFilter:GetBarsToFilter()
 	-- For every provider/target/bufftype combination,
 	-- call each provider-specific function to scan for the bar
 	for strProvider, tProviderDetails in pairs(BuffFilter.tBarProviders) do
-		if Apollo.GetAddon(strProvider) == nil then
-			--log:debug("Provider '%s' not found, skipping.", strProvider)
-		else
+		if Apollo.GetAddon(strProvider) ~= nil then 
 			for _,eTargetType in pairs(eTargetTypes) do
-				for _,eBuffType in pairs(eBuffTypes) do		
-					if tProviderDetails.fDiscoverBar ~= nil then
-						-- Translate bar target/bufftype properties to provider specific values
-						local strBarTypeParam = tProviderDetails.tTargetType[eTargetType]
+				local strBarTypeParam = tProviderDetails.tTargetType[eTargetType]
+				if strBarTypeParam ~= nil then
+					for _,eBuffType in pairs(eBuffTypes) do		
 						local strBuffTypeParam = tProviderDetails.tBuffType[eBuffType]
-						
-						if strBarTypeParam == nil or strBuffTypeParam == nil then
-							--log:debug("Provider '%s' does not support bar type '%s', buff type '%s'. Skipping.", strProvider, eTargetType, eBuffType)
-							break
-						end
-						
-						--log:debug("Scanning for %s/%s-bar on provider='%s'. Provider parameters: strBarTypeParam='%s' strBuffTypeParam='%s'", eTargetType, eBuffType, strProvider, strBarTypeParam, strBuffTypeParam)
-											
-						-- Safe call for provider-specific discovery function
-						local bStatus, discoveryResult = pcall(tProviderDetails.fDiscoverBar, strBarTypeParam, strBuffTypeParam)					
-						if bStatus == true and discoveryResult ~= nil then
-							--log:debug("%s/%s-bar found for provider '%s'", eTargetType, eBuffType, strProvider)
-						
-							-- Bar was found. Construct table with ref to bar, and provider-specific filter function.
-							local tFoundBar = {
-								eTargetType = eTargetType,					-- Target-type (Player, Target, Focus etc)
-								eBuffType = eBuffType,						-- Buff type (Buffs or Debuffs)
-								fFilterBar = tProviderDetails.fFilterBar,	-- Provider-specific filter function
-								bar = discoveryResult,						-- Reference to actual bar instance							
-							}
-												
-							-- Add found bar to result. Check remaining combos, more providers may be active at the same time, for the same bar
-							result[#result+1] = tFoundBar
+						if strBuffTypeParam ~= nil then																	
+							-- Safe call provider-specific discovery function
+							--log:debug("Scanning for '%s/%s'-bar on provider='%s'. Provider parameters: strBarTypeParam='%s' strBuffTypeParam='%s'", eTargetType, eBuffType, strProvider, strBarTypeParam, strBuffTypeParam)
+							local bStatus, discoveryResult = pcall(tProviderDetails.fDiscoverBar, strBarTypeParam, strBuffTypeParam)					
+							if bStatus == true and discoveryResult ~= nil then
+								-- Bar was found. Construct table with ref to bar, and provider-specific filter function.
+								--log:debug("%s/%s-bar found for provider '%s'", eTargetType, eBuffType, strProvider)							
+								local tFoundBar = {
+									eTargetType = eTargetType,					-- Target-type (Player, Target, Focus etc)
+									eBuffType = eBuffType,						-- Buff type (Buffs or Debuffs)
+									fFilterBar = tProviderDetails.fFilterBar,	-- Provider-specific filter function
+									bar = discoveryResult,						-- Reference to actual bar instance							
+								}
+								-- Add found bar to result. Check remaining combos, more providers may be active at the same time, for the same bar
+								result[#result+1] = tFoundBar
+							else
+								--log:debug("Unable to locate '%s/%s'-bar for provider '%s': %s", eTargetType, eBuffType, strProvider, tostring(discoveryResult))
+							end
 						else
-							--log:debug("Unable to locate %s/%s-bar for provider '%s': %s", eTargetType, eBuffType, strProvider, tostring(discoveryResult))
+							--log:debug("Provider '%s' does not support bar type '%s/%s'. Skipping.", strProvider, eTargetType, eBuffType)
 						end
 					end
+				else
+					--log:debug("Provider '%s' does not support bar type '%s'. Skipping.", strProvider, eTargetType, eBuffType)
 				end
 			end
+		else
+			--log:debug("Provider '%s' not found, skipping.", strProvider)
 		end
 	end
 	
